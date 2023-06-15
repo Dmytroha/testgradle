c2c26f50202773af368a50f31495a06a2f78e4c7
Dmytro Milovanov <milovanovu@gmail.com>
2023-06-15 20:01:34 +0200

    Lead code to execution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,16 @@
 package org.example;
 import org.example.dto.FullName;
 import com.google.gson.Gson;
-import org.apache.log4j.BasicConfigurator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static java.lang.System.*;
 
 
 public class Main {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
 
-        BasicConfigurator.configure();
+
         String jsonFullName = (new Gson()).toJson(new FullName("Dmytro", "Milovanov"));
-        LOGGER.info("{}",jsonFullName);
+
+        out.println(jsonFullName);
 
 
     }
